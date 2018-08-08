66f53b63c12f8647d4892c11b9ddeada279b0031
ハトネコエ (nekonenene) <hato.nekoe@gmail.com>
2018-08-08 21:17:40 +0900

    eslintの導入

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
-const webpack = require('webpack')
-const path = require('path')
-const globule = require('globule')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const webpack = require('webpack');
+const path = require('path');
+const globule = require('globule');
+const autoprefixer = require('autoprefixer');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 // 参考: https://qiita.com/toduq/items/2e0b08bb722736d7968c
@@ -10,8 +11,8 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 // ディレクトリの設定
 const dirs = {
   srcDir: path.join(__dirname, 'src'),
-  destDir: path.join(__dirname, 'dist')
-}
+  destDir: path.join(__dirname, 'dist'),
+};
 
 // keyの拡張子のファイルが、valueの拡張子のファイルに変換される
 const convertExtensions = {
@@ -19,50 +20,52 @@ const convertExtensions = {
   sass: 'css',
   scss: 'css',
   css: 'css',
-  js: 'js'
-}
+  js: 'js',
+};
 
 // トランスパイルするファイルを列挙する
 // _から始まるファイルは、他からimportされるためのファイルとして扱い、個別のファイルには出力しない
-const files = {}
-Object.keys(convertExtensions).forEach(from => {
-  const to = convertExtensions[from]
-  globule.find([`**/*.${from}`, `!**/_*.${from}`], {cwd: dirs.srcDir}).forEach(filename => {
-    files[filename.replace(new RegExp(`.${from}$`, 'i'), `.${to}`)] = path.join(dirs.srcDir, filename)
-  })
-})
+const files = {};
+Object.keys(convertExtensions).forEach((from) => {
+  const to = convertExtensions[from];
+  globule.find([`**/*.${from}`, `!**/_*.${from}`], { cwd: dirs.srcDir }).forEach((filename) => {
+    files[filename.replace(new RegExp(`.${from}$`, 'i'), `.${to}`)] = path.join(dirs.srcDir, filename);
+  });
+});
 
 // pugでトランスパイルする
 const pugLoader = [
   'apply-loader',
-  'pug-loader'
-]
+  'pug-loader',
+];
 
 // Sassをトランスパイルし、autoprefixerをかけるようにする
 const sassLoader = [
   {
     loader: 'css-loader',
     options: {
-      minimize: true
-    }
+      minimize: true,
+    },
   },
   {
     loader: 'postcss-loader',
     options: {
       ident: 'postcss',
-      plugins: (loader) => [require('autoprefixer')()]
-    }
+      plugins: () => [
+        autoprefixer(),
+      ],
+    },
   },
-  'sass-loader'
-]
+  'sass-loader',
+];
 
 // Babelでトランスパイルする
 const jsLoader = {
   loader: 'babel-loader',
   query: {
-    presets: ['env']
-  }
-}
+    presets: ['env'],
+  },
+};
 
 const config = {
   context: dirs.srcDir,
@@ -70,14 +73,14 @@ const config = {
 
   output: {
     filename: '[name]',
-    path: dirs.destDir
+    path: dirs.destDir,
   },
 
   module: {
     rules: [
       {
         test: /\.pug$/,
-        use: ExtractTextPlugin.extract(pugLoader)
+        use: ExtractTextPlugin.extract(pugLoader),
       },
       {
         test: /\.(sa|sc|c)ss$/,
@@ -85,53 +88,53 @@ const config = {
           {
             // pugから `require('./hoge.sass?inline')` のように呼ばれた時は、ExtractTextPluginをかけない
             resourceQuery: /inline/,
-            use: sassLoader
+            use: sassLoader,
           },
           {
             // それ以外の時は、単純にファイルを生成する
-            use: ExtractTextPlugin.extract(sassLoader)
-          }
-        ]
+            use: ExtractTextPlugin.extract(sassLoader),
+          },
+        ],
       },
       {
         test: /\.js$/,
         exclude: /node_modules(?!\/webpack-dev-server)/,
-        use: jsLoader
-      }
-    ]
+        use: jsLoader,
+      },
+    ],
   },
 
   plugins: [
     new ExtractTextPlugin('[name]'),
     // convertExtensionsに含まれていないファイルは、単純にコピーする
     new CopyWebpackPlugin(
-      [{from: {glob: '**/*', dot: true}}],
-      {ignore: Object.keys(convertExtensions).map((ext) => `*.${ext}`)}
+      [{ from: { glob: '**/*', dot: true } }],
+      { ignore: Object.keys(convertExtensions).map(ext => `*.${ext}`) },
     ),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    })
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
   ],
 
   devServer: {
     contentBase: dirs.destDir,
-    host: "localhost",
+    host: 'localhost',
     port: 8013,
-    watchContentBase: true
-  }
-}
+    watchContentBase: true,
+  },
+};
 
 if (process.env.NODE_ENV === 'production') {
   config.devtool = 'eval';
-  config.optimization = { minimizer: [new UglifyJsPlugin()] }
+  config.optimization = { minimizer: [new UglifyJsPlugin()] };
   config.plugins = config.plugins.concat([
     new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.AggressiveMergingPlugin()
-  ])
+    new webpack.optimize.AggressiveMergingPlugin(),
+  ]);
 }
 
 if (process.env.NODE_ENV === 'development') {
   config.devtool = 'source-map';
 }
 
-module.exports = config
+module.exports = config;
