3fa736ce507db8961ebfab009d89e818b281056b
ハトネコエ (nekonenene) <hato.nekoe@gmail.com>
2019-03-02 15:34:55 +0900

    ページ表示時にoutputに出力されるように

--- src/js/top.js
+++ src/js/top.js
@@ -1,21 +1,27 @@
 import Vue from 'vue';
 import { markdown } from 'markdown';
 
-console.log(markdown.parse('Hello **World**!'));
-
-window.onload = () => {
-  $('.output').html('出力部<br>');
-  console.log('start');
-};
+const defaultString = '# Hi!\n\nHello, **World**!\n\n```ruby\na = "abc"\nb = 2\n```\n\nThis is `code`.  \n`printf` is a method.\n\n`printf`はメソッドです。\n\n1. あれ\n2. それ\n3. どれ\n\n* あれ\n* それ\n* どれ\n';
 
 new Vue({
   el: '#app',
   data: {
-    inputText: '# Hi!\n\nHello **World**!',
+    inputText: defaultString,
   },
   watch: {
     inputText: function (val) {
       const parsed = markdown.parse(val);
+      console.log(parsed);
+      this.outputText = parsed.join('\n');
+    },
+  },
+  created: function () {
+    this.updateOutput();
+  },
+  methods: {
+    updateOutput: function () {
+      const str = this.inputText;
+      const parsed = markdown.parse(str);
       console.log(parsed);
       this.outputText = parsed.join('\n');
     },
